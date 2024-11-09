f0d884971b81a258d70d24d00527820c2b017a03
namskynet <158797778+namskynet@users.noreply.github.com>
2024-11-09 12:47:58 +1100

    assets added

--- server.ts
+++ server.ts
@@ -1,22 +1,42 @@
-const BASE_PATH = './web';
+const BASE_PATH = '.';
 const server = Bun.serve({
     port: 3000,
-    static: {   
+    static: {
         // serve a file by buffering it in memory
         "/": new Response(await Bun.file("./web/index.html").bytes(), {
-          headers: {
-            "Content-Type": "text/html",
-          },
+            headers: {
+                "Content-Type": "text/html",
+            },
         }),
-      },
-    
-       
-      async fetch(req) {
-        const filePath = BASE_PATH + new URL(req.url).pathname;
-        const file = Bun.file(filePath);
+        "/signin": new Response(await Bun.file("./web/signinpage.html").bytes(), {
+            headers: {
+                "Content-Type": "text/html",
+            },
+        }),
+        "/register": new Response(await Bun.file("./web/RegisterPage.html").bytes(), {
+            headers: {
+                "Content-Type": "text/html",
+            },
+        }),
 
-        return new Response(file);
-      },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+    },
+
+
+    async fetch(req) {
+        // Get asset files.
+        if (req.url.includes('/web/')) {
+            const filePath = BASE_PATH + new URL(req.url).pathname;
+            const file = Bun.file(filePath);
+            return new Response(file);
+        }
+
+        // API calls
+        if (req.url.includes('/api/')) {
+            // TODO add api calls.
+        }
+
+        return new Response("hoho");
+    },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
