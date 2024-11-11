38f55ce76c59c89938ad8a5bb42422bd3d2761a1
namskynet <158797778+namskynet@users.noreply.github.com>
2024-11-11 19:56:48 +1100

    Registration beta.

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import { Database } from "bun:sqlite";
 import { register } from './register';
 
+const db = new Database("movie-reviews.sqlite");
+
 const BASE_PATH = '.';
 const server = Bun.serve({
     port: 3000,
@@ -36,14 +38,28 @@ const server = Bun.serve({
         // API calls
         if (req.url.includes('/api/')) {
             // TODO add api calls.
-
-
             if (req.url.includes('/register')) {
-                register();
+
+
+                const formData = await req.formData();
+                const nickname = formData.get('nickname')?.toString()
+                const email = formData.get('email')?.toString()
+                const password = formData.get('password')?.toString()
+                if(!nickname||!email||!password) {
+                    throw new Error("not all credentials entered")
+                }
+
+                register(nickname, email, password)
+                return Response.redirect("/", 301);
+
+
+                debugger;
+
+
+                // const params = await req.text();
+                // const credentials = getRequestParam(params)
+                // register(credentials.nickname, credentials.email, credentials.password);
             }
-
-
-            
         }
 
         return new Response("hoho");
--- web/script.ts
+++ web/script.ts
@@ -1,2 +1,19 @@
 debugger;
 console.log('hello')
+
+
+const form = document.getElementById('register-form');
+if (!form) {
+    throw new Error("Form has not been created yet.");
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+
+
+})
+
+function onRegister() {
+
+}
